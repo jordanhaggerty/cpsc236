a8940b216f71dbe877df3bb263ed1674a540324a
jordanhaggerty <joeyhaggerty@live.com>
2025-02-28 21:06:26 -0500

    handsOn_3.py

--- handsOn_3.py
+++ handsOn_3.py
@@ -1,24 +1,22 @@
-# print("Even or Odd Checker")
-# print()
-# def user_input():
-#         num = int(input("Enter an integer: "))
-#         return num
+print("Even or Odd Checker")
+print()
+def user_input():
+        num = int(input("Enter an integer: "))
+        return num
 
-# def even_odd(num):
-#     if num % 2 == 0:
-#         print("This is an even number")
-#     elif num % 2 !=0:
-#         print("This is an odd number")
-#     else:
-#         print("invalid input")
-    
+def even_odd(num):
+    if num % 2 == 0:
+        print("This is an even number")
+    elif num % 2 !=0:
+        print("This is an odd number")
+    else:
+        print("invalid input")
 
-# def main():
-#     num = user_input()
-#     even_odd(num)
-
-# main() 
-
+def main():
+    num = user_input()
+    even_odd(num)
+main() 
+----------------------------------------------------
 import metersFeet as conv
 
 print("Feet and Meters Converter")
@@ -36,59 +34,56 @@ def main():
 
 if __name__ == "__main__":
     main()
+----------------------------------------------------
+import taxes as tax
 
-# import taxes as tax
+print("Sale Tax Calculator")
+print()
 
-# print("Sale Tax Calculator")
-# print()
-
-# def main():
-#     runagain = "y"
-#     while runagain.lower()  == "y":
-#         x = tax.item_cost()
-#         print(f"Total: $ {round(x,2)}")
-#         tax.tax(x)
-#         runagain = input("Again? (y/n): ")
-#         print()
-#         pass
-#     print("Thanks, bye!")
+def main():
+    runagain = "y"
+    while runagain.lower()  == "y":
+        x = tax.item_cost()
+        print(f"Total: $ {round(x,2)}")
+        tax.tax(x)
+        runagain = input("Again? (y/n): ")
+        print()
+        pass
+    print("Thanks, bye!")
    
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
+-----------------------------------------------------
+print("Prime number checker")
+print()
 
-# print("Prime number checker")
-# print()
+def user_input():
+    num = int(input("Please enter a number between 1 and 5000: "))
+    if num <= 1 or num > 5000:
+        print("invalid integer. Please try again")
+    else:
+        pass
+    return num
 
-# def user_input():
-#     num = int(input("Please enter a number between 1 and 5000: "))
-#     if num <= 1 or num > 5000:
-#         print("invalid integer. Please try again")
-#     else:
-#         pass
-#     return num
-
-# def prime(num):
-#     counter = 0
-#     for i in range(1,num+1):
-#         if num % i == 0:
-#             counter += 1
-#     if counter == 2:
-#         print(num, "is a prime number.")
-#     else:
-#         print(num, "Is not a prime number.")
-#         print(f"It has {counter} factors.")
-
+def prime(num):
+    counter = 0
+    for i in range(1,num+1):
+        if num % i == 0:
+            counter += 1
+    if counter == 2:
+        print(num, "is a prime number.")
+    else:
+        print(num, "Is not a prime number.")
+        print(f"It has {counter} factors.")
         
+def main():
+    input_again = "y"
+    while input_again.lower() == "y":
+        num = user_input()
+        prime(num)
+        input_again = input("Try Again? (y/n): ")
+        print()
+        pass
+    print("Bye!")
 
-
-# def main():
-#     input_again = "y"
-#     while input_again.lower() == "y":
-#         num = user_input()
-#         prime(num)
-#         input_again = input("Try Again? (y/n): ")
-#         print()
-#         pass
-#     print("Bye!")
-
-# main()
+main()
